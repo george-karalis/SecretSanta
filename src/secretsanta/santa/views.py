e6c952249a42f39316d29298f05b7ee97ab4754d
george-karalis <george.karalis@l3harris.com>
2024-11-17 13:30:55 +0000

    Creates Views
    * Update Group View
    * Delete Group View
    * Leave Group View

--- src/secretsanta/santa/views.py
+++ src/secretsanta/santa/views.py
@@ -1,16 +1,21 @@
+import random
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
 from django.views.generic import (
     CreateView,
+    DeleteView,
     DetailView,
     ListView,
+    UpdateView,
 )
 
 from . import forms, models
+from .utils import giver_receiver_matching
 
 
 class GroupListView(LoginRequiredMixin, ListView):
@@ -38,6 +43,73 @@ class GroupCreateView(LoginRequiredMixin, CreateView):
             gettext_lazy(f"Group {self.object.name} Created successfully!"),
         )
         return response
+
+
+class GroupUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = models.Group
+    template_name = "santa/group_from.html"
+    form_class = forms.GroupForm
+
+    # 1 TODO: Create group admin permission. Enable GroupMember's to be admins
+    # and allow group admins to update the group.
+    def test_func(self):
+        """
+        Allows only the group's creator from editing the group.
+
+        Returns
+        -------
+            True if the GroupMember is the creator.
+        """
+        group = self.get_object()
+        is_creator = self.request.user == group.created_by
+        return is_creator
+
+    def get_success_url(self):
+        return reverse_lazy("santa:group_detail", kwargs={"pk": self.object.pk})
+
+    def form_valid(self, form):
+        """
+        Prevents updating the Group after the matching is done.
+        """
+        if self.object.is_matched:
+            messages.error(
+                self.request,
+                gettext_lazy(
+                    f"Cannot edit {self.object.name} after the matching is completed"
+                ),
+            )
+            return redirect("santa:group_detail", pk=self.object.pk)
+        messages.success(
+            self.request,
+            gettext_lazy(f"Group {self.object.name} updated successfully."),
+        )
+        return super().form_valid(form)
+
+
+class GroupDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = models.Group
+    template_name = "santa/group_confirm_delete.html"
+    success_url = reverse_lazy("santa:group_list")
+
+    # 1 TODO: Create group admin permission. Enable GroupMember's to be admins
+    # and allow group admins to delete the group.
+    def test_func(self):
+        """
+        Allows only the group's creator from deleting the group.
+
+        Returns
+        -------
+            True if the GroupMember is the creator.
+        """
+        group = self.get_object()
+        is_creator = self.request.user == group.created_by
+        return is_creator
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(
+            request, gettext_lazy(f"Group {self.object.name} deleted successfully.")
+        )
+        return super().delete(request, *args, **kwargs)
 
 
 class GroupDetailView(LoginRequiredMixin, DetailView):
@@ -89,3 +161,36 @@ def update_wishlist(request, group_id):
         form = forms.WishListForm(instance=membership)
 
     return render(request, "santa/wishlist_form.html", {"form": form})
+
+
+@login_required
+def leave_group(request, group_id):
+    if request.method != "POST":
+        return redirect("santa:group_detail", pk=group_id)
+
+    group = get_object_or_404(models.Group, id=group_id)
+
+    # 1 TODO: Create group admin permission. Enable GroupMember's to be admins
+    # and update this view to not allow the last admin to leave the group.
+    if group.created_by == request.user:
+        messages.error(request, gettext_lazy("Group's creator cannot leave the group."))
+        return redirect("santa:group_detail", pk=group_id)
+
+    # 2 TODO: Allow GroupMembers to leave the group even after matching is done.
+    # Sent a notification to the GroupMember that had the removed member as recipient to redraw.
+    if group.is_matched:
+        messages.error(
+            request,
+            gettext_lazy("Cannot leave the group after the matching is completed."),
+        )
+        return redirect("santa:group_detail", pk=group_id)
+
+    member = get_object_or_404(models.GroupMember, user=request.user, group=group)
+    member_name = member.user.username
+    member.delete()
+
+    messages.success(
+        request,
+        gettext_lazy(f"{member_name} was successfully removed from {group.name}"),
+    )
+    return redirect("santa:group_list")
